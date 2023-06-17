79230450b7db7820feaa50298be2765077a7afd6
Shubham <shm.code1@gmail.com>
2023-06-17 22:27:25 +0530

    default argument file added

--- functions/7-defaultArgument.cpp
+++ functions/7-defaultArgument.cpp
@@ -4,7 +4,7 @@ int add(int x,int y,int z=0){
     return x+y+z;
 }
 int main(){
-    cout<<add(10,15)<<endl;
-    cout<<add(10,15,2);
+    cout<<add(10,15)<<endl;  // if two arguments passes it works
+    cout<<add(10,15,2);    // if three arguments passes it will work
     return 0;
 }
--- functions/7-defaultArgument2.cpp
+++ functions/7-defaultArgument2.cpp
@@ -4,8 +4,8 @@ int max(int a=0,int b=0,int c=0){
     return a>b && a>c?a:b>c?b:c;
 }
 int main(){
-    cout<<max()<<endl;
-    cout<<max(10)<<endl;
+    cout<<max()<<endl;    // here same function working with 4 different number of input as putted a=0,b=0,c=0
+    cout<<max(10)<<endl;   // if you want to fill default argument to the parameter then fill it from right hand side
     cout<<max(15,13)<<endl;
     cout<<max(15,13,17)<<endl;
     return 0;

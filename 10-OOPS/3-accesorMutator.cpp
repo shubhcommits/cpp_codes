9d5eecd4e58f2bb6407f885b3864d2e874cc2382
Shubham <shm.code1@gmail.com>
2023-06-19 19:40:47 +0530

    OOPS

---
+++ 10-OOPS/3-accesorMutator.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class Rectangle{
+    private :
+    int length;
+    int breadth;
+    public :
+    void setLength(int l){
+        if(l>=0){
+            length =l;
+        }
+        else{
+            length = 1;
+        }
+    }
+    void setBreadth(int l){
+        if(l>=0){
+            breadth =l;
+        }
+        else{
+            breadth = 1;
+        }
+    }
+    int getLength(){
+        return length;
+    }
+    int getBreadth(){
+        return breadth;
+    }
+    int area(){
+        return length*breadth;
+    }
+};
+int main(){
+    Rectangle r;
+    r.setLength(10);
+    r.setBreadth(5);
+    cout<<r.area()<<endl;
+    cout<<"Length is "<<r.getLength();
+}
---
+++ 10-OOPS/4-constructor.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+class Rectangle{
+    private :
+    int length;
+    int breadth;
+    public :
+    /*
+    Rectangle(){
+        length = 1;
+        breadth = 1;
+    }
+    */
+    Rectangle(int l=1,int b=1){
+        setLength(l);
+        setBreadth(b);
+    }
+    Rectangle(Rectangle &r){
+        length = r.length;
+        breadth = r.breadth;
+    }
+    void setLength(int l){
+        if(l>0){
+            length = l;
+        }
+        else{
+            length = 0;
+        }
+    }
+    void setBreadth(int b){
+        if(b>0){
+            breadth = b;
+        }
+        else{
+            breadth = 0;
+        }
+    }
+    int getLength(){
+        return length;
+    }
+    int getBreadth(){
+        return breadth;
+    }
+    int area(){
+        return length*breadth;
+    }
+};
+int main(){
+   Rectangle r1(10,5);
+   Rectangle r2(r1);
+   cout<<"Area is "<<r2.area()<<endl;
+}

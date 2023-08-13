3a4643bd68f053382130babb9c2ba49d12088df1
Shubham <shm.code1@gmail.com>
2023-08-13 11:15:59 +0530

    inheritence

--- practice.cpp
+++ practice.cpp
@@ -1,23 +1,54 @@
 #include<iostream>
 using namespace std;
-class Complex{
-    public:
-    int img;
-    int real;
-    Complex operator+(Complex x){
-        Complex temp;
-        temp.real = real+x.real;
-        temp.img = img+x.img;
-        return temp;
-    }
+class Rectangle{
+private:
+int length;
+int breadth;
+public:
+Rectangle();  //This is non paramaterise constructor
+Rectangle(int l,int b);
+int getLength(){return length;}
+int getBreadth(){return breadth;}
+void setLength(int l);
+void setBreadth(int b);
+int area();
+int perimeter();
+};
+class Cuboid : public Rectangle{
+private:
+int height;
+public:
+Cuboid(int h){
+    height=h;
+}
+int getHeight(){return height;}
+void setHeight(int h){height =h;}
+int volume(){return getLength()*getBreadth()*height;}
 };
 int main(){
-    Complex c1,c2,c3;
-    c1.real = 5;
-    c1.img = 3;
-    c2.real = 7;
-    c2.img = 15;
-    c3=c1+c2;
-    cout<<c3.real<<"+"<<c3.img<<endl;
-    return 0;
+    Cuboid c(5);
+    c.setLength(10);
+    c.setBreadth(5);
+    cout<<c.volume();
+
+}
+Rectangle::Rectangle(){
+    length =1;
+    breadth=1;
+}
+Rectangle::Rectangle(int l,int b){
+    length=l;
+    breadth=b;
+}
+void Rectangle::setLength(int l){
+    length=l;
+}
+void Rectangle::setBreadth(int b){
+    breadth=b;
+}
+int Rectangle::area(){
+    return length*breadth;
+}
+int Rectangle::perimeter(){
+    return 2*(length+breadth);
 }

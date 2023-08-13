8ec62af2d88c621409939d8bfd86788e45f05ea2
Shubham <shm.code1@gmail.com>
2023-08-13 14:11:40 +0530

    constructor inheritence

--- practice.cpp
+++ practice.cpp
@@ -1,54 +1,30 @@
 #include<iostream>
 using namespace std;
-class Rectangle{
-private:
-int length;
-int breadth;
-public:
-Rectangle();  //This is non paramaterise constructor
-Rectangle(int l,int b);
-int getLength(){return length;}
-int getBreadth(){return breadth;}
-void setLength(int l);
-void setBreadth(int b);
-int area();
-int perimeter();
+class Base{
+    public:
+    Base(){
+        cout<<"Defaut of Base"<<endl;
+    }
+    Base(int x){
+        cout<<"Constructor of Base"<<endl;
+    }
+    Base(int x,int y){
+        cout<<"Constructor of Base"<<endl;
+    }
 };
-class Cuboid : public Rectangle{
-private:
-int height;
-public:
-Cuboid(int h){
-    height=h;
-}
-int getHeight(){return height;}
-void setHeight(int h){height =h;}
-int volume(){return getLength()*getBreadth()*height;}
+class derived:public Base{
+    public:
+    derived(){
+        cout<<"Default of derived"<<endl;
+    }
+    derived(int x){
+        cout<<"Constructor of derived"<<endl;
+    }
+     derived(int x,int y):Base(x){   // Here  :Base is  
+     // used to call constructor of base
+        cout<<"Constructor of derived"<<endl;
+    }
 };
 int main(){
-    Cuboid c(5);
-    c.setLength(10);
-    c.setBreadth(5);
-    cout<<c.volume();
-
-}
-Rectangle::Rectangle(){
-    length =1;
-    breadth=1;
-}
-Rectangle::Rectangle(int l,int b){
-    length=l;
-    breadth=b;
-}
-void Rectangle::setLength(int l){
-    length=l;
-}
-void Rectangle::setBreadth(int b){
-    breadth=b;
-}
-int Rectangle::area(){
-    return length*breadth;
-}
-int Rectangle::perimeter(){
-    return 2*(length+breadth);
+    derived c(10,5);
 }

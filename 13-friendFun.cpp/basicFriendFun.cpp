c36ef6ea646d8b94a11536c38511d535fe98f5c3
Shubham <shm.code1@gmail.com>
2023-08-18 19:25:29 +0530

    friend function

---
+++ 13-friendFun.cpp/basicFriendFun.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class Test{
+    private:
+    int a;
+    protected:
+    int b;
+    public:
+    int c;
+    friend int main();
+};
+int main(){
+    Test t;
+   // t.a=10; // not sccessible
+   // t.b=20; // not accessible
+   t.a=10;  // this is now accessible because class friend function is declared
+   t.b=20;  // this is now accessible because class friend function is declared
+    t.c=30;
+}
---
+++ 13-friendFun.cpp/friendClass.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class your;
+class My{
+    private:
+    int a=5;
+    friend your;
+};
+class your{
+    public:
+    My b;  // Object of class My
+    void  fun1(){
+        cout<<b.a;
+    }
+};
+
